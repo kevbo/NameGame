0fe5bec77e3ad320ff3ec7b169ff1bf79a5e4d55
Kevin Boers <kboers@leapfrogonline.com>
2014-01-08 11:57:04 -0600

    Use string templating, and different slicing

--- NameGame.py
+++ NameGame.py
@@ -8,12 +8,22 @@ length = len(name)
 print "Name Variable = " + name
 print "Length variable (length of name variable) = " + str(length)
 
+# the slicing syntax below ([1:]) means
+# 'from the second character until the end'
+shortened_name = name[1:]
+
+values = {'name': name, 'shortened_name': shortened_name}
+
 if name == "Chuck" or name == "chuck" or name == "CHUCK":
     subprocess.call(['say', "You are naughty! I am not doing that one!"])
 else:
-    subprocess.call(['say', name + "," + name + "," + "Bo " + "B" + name[1:length] + "."])
+    subprocess.call(
+        ['say', '{name}, {name}, Bo B{shortened_name}.'.format(**values)])
 
-    subprocess.call(['say', "Banana Fan uh. Fo F" + name[1:length] + "."])
+    subprocess.call(
+        ['say', "Banana Fan uh. Fo F{shortened_name}.".format(**values)])
 
-    subprocess.call(['say', "Meet my, mo M" + name[1:length] + "."])
+    subprocess.call(
+        ['say', "Meet my, mo M{shortened_name}.".format(**values)])
+
     subprocess.call(['say', name + "!"])
