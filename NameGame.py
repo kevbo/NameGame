84e1bb448a73cdd3322e1d77ed573d1fc6d0af7b
Kevin Boers <kboers@leapfrogonline.com>
2014-01-08 10:42:20 -0600

    Syntax and PEP8 fixes

--- NameGame.py
+++ NameGame.py
@@ -1,3 +1,6 @@
+import subprocess
+
+
 name = "Matt"
 
 length = len(name)
@@ -5,14 +8,13 @@ length = len(name)
 print "Name Variable = " + name
 print "Length variable (length of name variable) = " + str(length)
 
-import subprocess
 
-if name == "Chuck" or "chuck" or "CHUCK":
+if name in ("Chuck", "chuck", "CHUCK"):
     subprocess.call(['say', "You are naughty! I am not doing that one!"])
 else:
-    subprocess.call(['say', name + "," + name + "," + "Bo " + "B" + name[1:length] + "."]) 
+    subprocess.call(['say', name + "," + name + "," + "Bo " + "B" + name[1:length] + "."])
 
-    subprocess.call(['say', "Banana Fan uh. Fo F" + name[1:length] + "."]) 
+    subprocess.call(['say', "Banana Fan uh. Fo F" + name[1:length] + "."])
 
     subprocess.call(['say', "Meet my, mo M" + name[1:length] + "."])
-    subprocess.call(['say', name+"!" ])
+    subprocess.call(['say', name + "!"])
